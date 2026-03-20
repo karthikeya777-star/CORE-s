c4a47669448e4a5fc05875ca57e62a2adfc15cb3
Karthhikeya Vemula <karthhikeya031007@gmail.com>
2026-03-20 15:16:26 +0530

    Add car data to script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const cars = [
+  {
+    id: 1,
+    name: "Tesla Model 3",
+    price: "₹60 Lakhs",
+    engine: "Electric",
+    mileage: "500 km",
+    description: "Electric sedan with autopilot"
+  },
+  {
+    id: 2,
+    name: "Hyundai Creta",
+    price: "₹15 Lakhs",
+    engine: "Petrol",
+    mileage: "17 kmpl",
+    description: "Popular SUV in India"
+  }
+];
